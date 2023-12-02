f1a1d45f07a583e20bae1051f5ea84b2583be1fb
Dmitry Sidorov <61616100+JustyDev@users.noreply.github.com>
2023-12-02 19:04:22 +0300

    Initial Commit

---
+++ JustyMusic/JustyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustyMusicApp.swift
+//  JustyMusic
+//
+//  Created by Дмитрий Сидоров on 02.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct JustyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
